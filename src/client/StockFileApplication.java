3480a7c34cf7400e88245d64b94f00a56aa5258d
omer.cavas <omer.cavas@epias.com.tr>
2023-01-10 12:53:36 +0300

    Add existing project files to Git

---
+++ src/client/StockFileApplication.java
@@ -0,0 +1,50 @@
+package client;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import fileprocessors.StockFileData;
+import fileprocessors.StockFileReader;
+
+public class StockFileApplication {
+	
+	public static void main(String args[]) throws IOException{
+		StockFileReader fr = new StockFileReader("table.csv");
+		
+		List<HashMap<String, Double>> dataResult = populateStockFileData(fr.getHeaders(), fr.readFileData());
+		StockFileData fileData = new StockFileData();
+		fileData.addData(dataResult);
+		fileData.printData();
+		System.out.println(dataResult.size()); // 251 data
+	}
+	/**
+	 * Complete the method body so that it returns the given structure needed to 
+	 * populate the data field in the StockFileData class. 
+	 * @param headers
+	 * @param lines
+	 * @return List
+	 */
+	public static List<HashMap<String, Double>> populateStockFileData(List<String> headers, List<String> lines){
+		List<HashMap<String, Double>> dataResult = new ArrayList<>();
+		// Insert your code here..
+		
+		for(String line:lines) {
+			
+			String[] cells=line.split(",");
+			HashMap<String,Double> entry=new HashMap<>();
+			for(int i=0;i<cells.length;i++) {
+				entry.put(headers.get(i),Double.parseDouble(cells[i]));
+			}
+			dataResult.add(entry);
+			
+			
+		}
+		
+		
+		return dataResult;
+	}
+	
+	
+}
